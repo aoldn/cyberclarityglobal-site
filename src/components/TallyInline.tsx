153860045c54c95d34aadd077f7b3f39914d60a6
aoldn <75085750+aoldn@users.noreply.github.com>
2025-10-27 14:39:01 +0200

    Update TallyInline iframe source URL

--- src/components/TallyInline.tsx
+++ src/components/TallyInline.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function TallyInline() {
   return (
     <iframe
-      src="https://tally.so/r/ABC123?hideTitle=1&transparentBackground=1&alignLeft=1"
+      src="https://tally.so/r/mDl82b?hideTitle=1&transparentBackground=1&alignLeft=1"
       width="100%"
       height="1200"
       frameBorder="0"
